6f590efb39834ea17054dba7a2caa7a3702c52a6
artificial-life <proteus.protocol@gmail.com>
2016-07-22 18:03:45 +0600

    upd

--- src/classes/workstations/ReceptionWorkstation.js
+++ src/classes/workstations/ReceptionWorkstation.js
@@ -4,11 +4,31 @@ let BaseWorkstation = require('./BaseWorkstation.js');
 let Connection = require('../access-objects/connection-instance.js');
 
 let connection = new Connection();
-
+let SharedEntities = require('../access-objects/SharedEntities.js');
 
 class ReceptionWorkstation extends BaseWorkstation {
 	constructor(user) {
 		super(user, 'reception');
+	}
+	getShared() {
+		let ws_params = {
+			workstation: this.getId()
+		};
+		let request_shared = [{
+			name: 'timezone',
+			params: ws_params
+		}, {
+			name: 'office',
+			params: ws_params
+		}, {
+			name: 'services',
+			params: ws_params
+		}, {
+			name: 'organization-chain',
+			params: ws_params
+		}];
+
+		return SharedEntities.request(request_shared);
 	}
 	getServiceInfo(params) {
 		return connection.request('/reception/service-info', params);
--- src/classes/workstations/ReportsWorkstation.js
+++ src/classes/workstations/ReportsWorkstation.js
@@ -4,15 +4,35 @@ let BaseWorkstation = require('./BaseWorkstation.js');
 let Connection = require('../access-objects/connection-instance.js');
 
 let connection = new Connection();
+let SharedEntities = require('../access-objects/SharedEntities.js');
 
-
-class ReportWorkstation extends BaseWorkstation {
+class ReportsWorkstation extends BaseWorkstation {
 	constructor(user) {
 		super(user, 'reports');
+	}
+	getShared() {
+		let ws_params = {
+			workstation: this.getId()
+		};
+		let request_shared = [{
+			name: 'timezone',
+			params: ws_params
+		}, {
+			name: 'office',
+			params: ws_params
+		}, {
+			name: 'services',
+			params: ws_params
+		}, {
+			name: 'organization-chain',
+			params: ws_params
+		}];
+
+		return SharedEntities.request(request_shared);
 	}
 	getTable(template) {
 		return connection.request('/reports/get-table', template, 'http').then(r => r.value);
 	}
 }
 
-module.exports = ReportWorkstation;
+module.exports = ReportsWorkstation;
